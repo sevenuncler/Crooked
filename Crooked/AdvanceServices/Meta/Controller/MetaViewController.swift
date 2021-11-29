f02968e87b52d3676adbae90d9342646543cd599
方赫 <fanghe@bytedance.com>
2021-11-29 13:43:42 +0800

    init
    init

---
+++ Crooked/AdvanceServices/Meta/Controller/MetaViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MetaViewController.swift
+//  Crooked
+//
+//  Created by fanghe on 2021/11/29.
+//
+
+import UIKit
+
+class MetaViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
